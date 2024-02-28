9b0aff74c11b13d8c20e7e5b2382bf7b2b8b3929
Ebrahim-78 <144029665+Ebrahim-78@users.noreply.github.com>
2024-02-28 20:10:53 +0300

    add Link tag to the Title,body,priority

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Link from 'next/link'
 async function getTickets(){
     const res = await fetch('http://localhost:4000/tickets', {
         next:{
@@ -15,11 +15,13 @@ export default async function TicketList() {
         <>
             {tickets.map((ticket) => (
                 <div key={ticket.id} className='card my-5'>
+                    <Link href={`/tickets/${ticket.id}`}>
                     <h3>{ticket.title}</h3>
                     <p>{ticket.body.slice(0,200)}...</p>
                     <div className={`pill ${ticket.priority}`}>
                         {ticket.priority} priority
                     </div>
+                    </Link>
                 </div>
             ))}
             {tickets.length === 0 && (
